b17ba1d59dae617f2188fc6d6a244b7914d824aa
Bakugan1024 <85323092+Bakugan1024@users.noreply.github.com>
2021-12-17 17:25:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,103 +1,103 @@
-canvas = document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
-rover_width = 100;
-rover_height = 90;
-background_image = "mars.jpg";
-rover_image = "rover.png";
-rover_x = 10;
-rover_y = 10;
+//Create a reference for canvas 
+canvas=document.getElementById('myCanvas')
+ctx=canvas.getContext("2d")
+//Give specific height and width to the car image
+greencar_width=75;
+greencar_height=100;
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+greencar_x=5;
+greencar_y=225;
 
 function add() {
 	background_imgTag = new Image(); //defining a variable with a new image
 	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
 	background_imgTag.src = background_image; // load image
 
-	rover_imgTag = new Image(); //defining a variable with a new image
-	rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
-	rover_imgTag.src = rover_image; // load image
+	greencar_imgTag = new Image(); //defining a variable with a new image
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;	
 }
 
 function uploadBackground() {
 	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
 }
 
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
 
-
-function uploadrover() {
-	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+	
 }
-
-
-
-
 
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e) {
+function my_keydown(e)
+{
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	if (keyPressed == '38') {
-		up();
-		console.log("up");
-	}
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
 
-	if (keyPressed == '40') {
-		down();
-		console.log("down");
-	}
+function up()
+{
+	if (greencar_y >= 100) {
+		greencar_y = greencar_y - 10;
+		uploadBackground();
+		uploadgreencar();
 
-	if (keyPressed == '37') {
-		left();
-		console.log("left");
 	}
+}
+function down()
+{
+if (greencar_y <= 500) {
+	greencar_y = greencar_y + 10;
+		uploadBackground();
+		uploadgreencar();
 
-	if (keyPressed == '39') {
-		right();
-		console.log("right");
+	}
+}
+function left()
+{
+	if (greencar_x >= 0) {
+		greencar_x = greencar_x - 10;
+		uploadBackground();
+		uploadgreencar();
+
 	}
 }
 
-
-function right() {
-	if (rover_x <= 700) {
-		rover_x = rover_x + 10;
+function right()
+{
+	if (greencar_x <= 700) {
+		greencar_x = greencar_x + 10;
 		uploadBackground();
-		uploadrover();
+		uploadgreencar();
 	}
-
-
-}
-
-
-function left() {
-	if (rover_x >= 0) {
-		rover_x = rover_x - 10;
-		uploadBackground();
-		uploadrover();
-	}
-
-
-}
-
-
-function up() {
-	if (rover_y >= 100) {
-		rover_y = rover_y - 10;
-		uploadBackground();
-		uploadrover();
-	}
-
-
-}
-
-
-function down() {
-	if (rover_y <= 500) {
-		rover_y = rover_y + 10;
-		uploadBackground();
-		uploadrover();
-	}
-
-
 }
